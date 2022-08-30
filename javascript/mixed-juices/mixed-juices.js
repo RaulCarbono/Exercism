199dfd66133611be2b59230d8869c040e2214b63
RaulCarbono <carbonoraul2@gmail.com>
2022-08-29 21:01:23 -0500

    my solution

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -10,9 +10,31 @@
  * @param {string} name
  * @returns {number} time in minutes
  */
-export function timeToMixJuice(name) {
-  wile (name === "Pure Strawberry Joy");
-    return 0.5
+export function timeToMixJuice(name){
+  switch (name){
+  case 'Pure Strawberry Joy':
+  return 0.5
+  break;
+      
+    case 'Energizer':
+      return 1.5
+        break
+
+    case 'Green Garden':
+      return 1.5
+        break
+
+    case 'Tropical Island':
+      return 3 
+      break
+
+    case 'All or Nothing':
+      return 5
+      break
+
+    default: 
+      return 2.5
+}
 }
 
 /**
@@ -24,7 +46,35 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+ let count1 = 0
+ let count2 = 0
+ let i = 0
+  while (i<= limes.length){
+    if (wedgesNeeded == 0){
+      return 0
+    }
+    switch (limes[i]){
+    case 'small':
+    count1 += 6
+    count2 += 1
+    break;
+      
+    case 'medium':
+      count1 += 8
+      count2 += 1 
+        break
+
+    case 'large':
+      count1 += 10
+      count2 += 1 
+        break  
+}
+    i++
+    if (count1 >= wedgesNeeded){
+      return count2
+    } 
+}
+  return limes.length
 }
 
 /**
@@ -35,5 +85,49 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+
+ let times = 0
+ let count = 0
+ let i = 0
+  while (i<= orders.length){
+    
+    switch (orders[i]){
+    case 'Pure Strawberry Joy':
+    times += 0.5
+    count += 1
+    break;
+      
+    case 'Energizer':
+      times += 1.5
+      count += 1
+        break
+
+    case 'Green Garden':
+      times += 1.5
+      count += 1
+        break
+
+    case 'Tropical Island':
+      times += 3
+        count +=1
+      break
+
+    case 'All or Nothing':
+      times += 5
+        count += 1
+      break
+        
+      default: 
+      times += 2.5
+      count +=1
 }
+    i++
+    
+    console.log(count)
+    if (times >= timeLeft){
+      return orders.slice(count, orders.length )
+    } 
+}
+  
+}
+ 
